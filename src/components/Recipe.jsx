dd16670f7a78389c4098117494f9ac9bde3a9c2e
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-01-08 11:46:33 +0100

    add img

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,6 +1,7 @@
 
-export const Recipe = ({ recipe: { name } }) => {
+export const Recipe = ({ recipe: { name, image } }) => {
     return <div>
         <h2>{name}</h2>
+        <img src={image} alt={name} width='240'/>
     </div>
 }
--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -2,7 +2,7 @@ import { Recipe } from './Recipe';
 
 export const RecipeList = ({ recipes }) => {
   return (
-    <div>
+    <div style={{display: 'flex', gap: 16}}>
       {recipes.map(recipe => (
         <Recipe key={recipe.id} recipe={recipe} />
       ))}
